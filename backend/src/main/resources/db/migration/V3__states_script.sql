3035ad3cc2a3071fd4d7ba3eafbcf4f09ee0707f
eyasser <eyasser@ntgclarity.com>
2024-04-03 23:04:20 +0200

    add file migrte DB to insert states

---
+++ backend/src/main/resources/db/migration/V3__states_script.sql
@@ -0,0 +1,34 @@
+SELECT 3 AS baseline;
+
+
+
+INSERT INTO states (name)
+VALUES ('Alexandria'),
+       ('Aswan'),
+       ('Asyut'),
+       ('Beheira'),
+       ('BeniSuef'),
+       ('Cairo'),
+       ('Dakahlia'),
+       ('Damietta'),
+       ('Faiyum'),
+       ('Gharbia'),
+       ('Giza'),
+       ('Ismailia'),
+       ('Kafr El Sheikh'),
+       ('Luxor'),
+       ('Matruh'),
+       ('Minya'),
+       ('Monufia'),
+       ('New Valley'),
+       ('North Sinai'),
+       ('Port Said'),
+       ('Qalyubia'),
+       ('Qena'),
+       ('Red Sea'),
+       ('Sharqia'),
+       ('South Sinai'),
+       ('Suez');
+
+
+
