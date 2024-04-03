23782ab43d3ef3a2e63c59be67a6f5bc4d272df4
eyasser <eyasser@ntgclarity.com>
2024-04-03 22:11:26 +0200

    add migrateions script to insert data

---
+++ backend/src/main/resources/db/migration/V1__category_script.sql
@@ -0,0 +1,22 @@
+-- -----------------------------------------------------
+-- Categories
+-- -----------------------------------------------------
+
+SELECT 1 AS baseline;
+
+
+INSERT INTO category(name, description, image, logo,date_create)
+	VALUES ('Foods', 'Chef with diligence finishing dish on plate', 'food', 'fas fa-solid fa-pot-food',now());
+
+    INSERT INTO category(name, description, image, logo,date_create)
+	VALUES ('Cold Drinks', 'Group of Soft drinks bottles. Bottled beverage', 'colddrinks', 'fas fa-solid fa-wine-bottle',now());
+
+    INSERT INTO category(name, description, image, logo,date_create)
+	VALUES ('Hot Drinks', 'Group of  Coffee and Tea ,milks','hotdrinks', 'fas fa-solid fa-mug-hot',now());
+
+    INSERT INTO category(name, description, image, logo,date_create)
+	VALUES ('Sweets', 'Group of konafa in a tray with pistachio', 'sweets', 'fa-solid fa-cookie-bite',now());
+
+    INSERT INTO category(name, description, image, logo,date_create)
+	VALUES ('Extras', 'Group of Sweet and Salty Snacks, Perfect ', 'extras', 'fa-solid fa-road-spikes',now());
+
