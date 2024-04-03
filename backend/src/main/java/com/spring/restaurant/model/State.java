024de420003aead3f3c304f12bed7363889ceac3
eyasser <eyasser@ntgclarity.com>
2024-04-03 23:17:22 +0200

    delete inhertance in state model

--- backend/src/main/java/com/spring/restaurant/model/State.java
+++ backend/src/main/java/com/spring/restaurant/model/State.java
@@ -2,14 +2,12 @@ package com.spring.restaurant.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
 @Table(name = "states")
-public class State extends PublicData {
-
+public class State {
+    @Column(name = "name")
+    private String name;
 }
