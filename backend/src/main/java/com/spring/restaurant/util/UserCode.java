fec5059b1973a63dba0703ed3e5d24628850e5fe
eyasser <eyasser@ntgclarity.com>
2024-04-05 15:25:36 +0200

    simple Activation code which send to mail

--- backend/src/main/java/com/spring/restaurant/util/UserCode.java
+++ backend/src/main/java/com/spring/restaurant/util/UserCode.java
@@ -1,11 +1,18 @@
 package com.spring.restaurant.util;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class UserCode {
 
     public static String getCode() {
-        return UUID.randomUUID().toString();
+
+//        String x =  UUID.randomUUID().toString();
+
+        Random random = new Random();
+        int code = random.nextInt(900000) + 100000;
+        return String.valueOf(code);
+
     }
 
 }
