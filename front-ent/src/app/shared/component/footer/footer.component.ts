e7a638b0258468ced3e63c6811a85ac295479eb8
eyasser <eyasser@ntgclarity.com>
2024-04-06 14:04:49 +0200

    Refactor and Cancel @input from app Component to footer

--- front-ent/src/app/shared/component/footer/footer.component.ts
+++ front-ent/src/app/shared/component/footer/footer.component.ts
@@ -10,10 +10,52 @@ import { NgFor, NgIf } from '@angular/common';
     imports: [NgFor, NgIf]
 })
 export class FooterComponent {
-  @Input()
-  footerSocialData: Array<FooterInfo> | undefined;
-  @Input()
-  footerOpenDays: Array<FooterInfo> | undefined;
-  @Input()
-  footerContactDetails: Array<FooterInfo> | undefined;
+
+  socialMediaInformation: Array<FooterInfo> = [
+    {
+      socialMediaIcon: ' pi pi-facebook',
+      contactDetailsHerf: 'https://www.facebook.com/ntgclaritynetworks',
+      title: 'facebook'
+    },
+    {
+      socialMediaIcon: 'pi pi-google',
+      contactDetailsHerf: 'https://ntgclarity.com/',
+      title: 'google'
+    },
+    {
+      socialMediaIcon: 'pi pi-linkedin',
+      contactDetailsHerf: 'https://www.linkedin.com/company/ntg-clarity/mycompany/verification/',
+      title: 'linkedin'
+    },
+    {
+      socialMediaIcon: 'pi pi-twitter',
+      contactDetailsHerf: 'https://twitter.com/NTGClarityStage',
+      title: 'twitter'
+    },
+    {
+      socialMediaIcon: 'pi pi-instagram',
+      contactDetailsHerf: 'https://www.instagram.com/explore/locations/1017721966/ntg-clarity-networks-inc/',
+      title: 'instagram'
+    }
+  ];
+
+  openDaysInformation: Array<FooterInfo> = [
+    {openDays: 'Everyday'},
+    {openDays: '10.00 Am -10.00 Pm'},
+  ];
+  contactDetailsInformation: Array<FooterInfo> = [
+    {contactDetailsIcon: 'pi pi-map-marker', contactDetailsCommand: 'location ', contactDetailsHerf: '#'},
+    {contactDetailsIcon: 'pi pi-phone', contactDetailsCommand: '01092190082', contactDetailsHerf: '#'},
+    {
+      contactDetailsIcon: 'pi pi-globe',
+      contactDetailsCommand: 'https://ntgclarity.com/ ',
+      contactDetailsHerf: 'https://ntgclarity.com/'
+    },
+    {
+      contactDetailsIcon: 'pi pi-inbox',
+      contactDetailsCommand: 'ntgapps@ntgclarity.com',
+      contactDetailsHerf: 'https://accounts.google.com/v3/signin/identifier?dsh=S-569605920%3A1679139941662845&authuser=0&continue=https%3A%2F%2Fmail.google.com&ec=GAlAFw&hl=ar&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession'
+    },
+  ];
+
 }
