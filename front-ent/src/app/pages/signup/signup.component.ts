d34f8718ba505da7057209fc4aa55e4830d24437
eyasser <eyasser@ntgclarity.com>
2024-04-12 02:50:50 +0200

    refactor page Sign Up (refactor Code and add more Styles add cancel button )

--- front-ent/src/app/pages/signup/signup.component.ts
+++ front-ent/src/app/pages/signup/signup.component.ts
@@ -1,41 +1,60 @@
 import {Component, OnInit} from '@angular/core';
-import { FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
+import {FormBuilder, Validators, ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {LoginUserInformationService} from "../../shared/services/login-user-information.service";
-import { Router, RouterLink } from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {AuthenticationService} from "../../shared/services/security/authentication.service";
 import {error} from "@angular/compiler-cli/src/transformers/util";
 import {SpaceValidator} from "../../model/space-validator";
-import { RippleModule } from 'primeng/ripple';
-import { ButtonModule } from 'primeng/button';
-import { NgIf } from '@angular/common';
-import { InputTextModule } from 'primeng/inputtext';
-
+import {NgForOf, NgIf} from '@angular/common';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {InputGroupModule} from "primeng/inputgroup";
+import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {PasswordModule} from "primeng/password";
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
-    selector: 'app-signup',
-    templateUrl: './signup.component.html',
-    styleUrls: ['./signup.component.scss'],
-    standalone: true,
-    imports: [ReactiveFormsModule, RouterLink, InputTextModule, NgIf, ButtonModule, RippleModule]
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, InputTextModule, NgIf, ButtonModule, RippleModule, InputGroupModule, InputGroupAddonModule, RadioButtonModule, FormsModule, NgForOf, PasswordModule,TooltipModule]
 })
 export class SignupComponent implements OnInit {
   signupForm: any;
+  isLoading: boolean = false;
+  genderList: any[] = [
+    {name: 'Male', value: 1},
+    {name: 'Female', value: 2},
+  ];
+  selectedGenderType: any = null;
+  regexForMobNum = '(^((\\+?)\\d{1,3}[- ]?)?\\d{10,11}$)';
+  mobileNumberValidation: string = 'Phone Number must be follow patterns \n ' +
+    '( 10 digits Mobile Number ) Like 8087339090          \n'+
+    ' OR ( 11 digits Mobile Number ) Like 08087339090          \n'+
+    ' OR ( {+1,+91,+912} + 10 digits ) Like +1 8087339090     \n'+
+    ' OR ( {+1-,+91-,+912-} + 10 digits ) Like +1-8087339090  \n' ;
+   userMobilePhone: any;
+   validMobilePhone: boolean=false;
+
   constructor(
-    private _router: Router,
-    private _formBuilder: FormBuilder,
-    // private _http: HttpClient,
-    private authService: AuthenticationService
-  ) {
+    private _router: Router,private _formBuilder: FormBuilder,private authService: AuthenticationService) {
+
   }
 
   ngOnInit(): void {
     this.mySignupForm()
-
+    this.selectedGenderType = this.genderList[1];
   }
 
   mySignupForm() {
     this.signupForm = this._formBuilder.group({
+      UserName: [
+        '',
+      ],
       Email: [
         '',
         [Validators.required,
@@ -49,34 +68,78 @@ export class SignupComponent implements OnInit {
           Validators.minLength(5),
 
         ]
+      ],
+      ConfirmPassword: [
+        '',
+        [Validators.required,
+          Validators.minLength(5),
+
+        ]
       ]
     });
   }
 
 
   done() {
+    this.isLoading = true;
     this.authService.createUser(
       this.signupForm.controls['Email'].value, this.signupForm.controls['Password'].value)
       .subscribe({
-      next: response => {
-        // alert(this.signupForm.controls['Email'].value)
-        // alert(this.signupForm.controls['Password'].value)
-        if (response.result == 1) {
-          sessionStorage.setItem('account',this.signupForm.controls['Email'].value);
-          this._router.navigate(['signup/active']);
-        } else {
-          alert('Email is Exist');
+        next: response => {
+          // alert(this.signupForm.controls['Email'].value)
+          // alert(this.signupForm.controls['Password'].value)
+          if (response.result == 1) {
+            sessionStorage.setItem('account', this.signupForm.controls['Email'].value);
+            this._router.navigate(['signup/active']);
+          } else {
+            this.isLoading = false;
+            alert('Email is Exist');
+          }
+
+        }, error: error => {
+          this.isLoading = false
+          alert("Email or Password is invaild")
+
         }
 
-      }, error: error => {
-        alert("Email or Password is invaild")
+      });
 
-      }
-
-    });
-
+    setTimeout(() => {
+      this.isLoading = false
+    }, 2000);
 
   }
+
+
+  Cancel() {
+    this._router.navigate(['login']);
+  }
+
+  onChangeGenderType(event:any) {
+    this.selectedGenderType=event;
+  }
+
+
+  onChangeMobileNumber(event:any) {
+    debugger
+    this.userMobilePhone = event?.target?.value;
+  }
+
+  private checkEmployeeMobilePhoneValidation() {
+    this.validMobilePhone = this.userMobilePhone?.Mobile_Phone?.match(/^((\+?)\d{1,3}[- ]?)?\d{10,11}$/) &&
+      !(this.userMobilePhone.Mobile_Phone.match(/0{5,}/));
+    if(this.validMobilePhone) {
+      return true;
+    } else {
+      // show alert
+      return false
+    }
+  }
+
+
+
+
+
 
 
 }
