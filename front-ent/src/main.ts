c1a11f0eff98876f0c3dad8538136e88ac8647f1
Ezzat Yaser <eyasser@ntgclarity.com>
2025-09-27 03:35:49 +0300

    Revert "comment dropdown as i unused in primeng 20"
    This reverts commit c692c55c

--- front-ent/src/main.ts
+++ front-ent/src/main.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app/app.component';
 import { RippleModule } from 'primeng/ripple';
 import { MessageModule } from 'primeng/message';
 import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { ChipsModule } from 'primeng/chips';
 import { ImageModule } from 'primeng/image';
 import { ButtonModule } from 'primeng/button';
 import { SharedModule } from './app/shared/shared.module';
@@ -18,12 +20,13 @@ import { HttpIntercepterBaseAuthService } from './app/shared/services/security/h
 import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { OrderService } from './app/shared/services/order.service';
+import { PrimeNGConfig } from 'primeng/api';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, SharedModule, ButtonModule, ImageModule, FormsModule, CheckboxModule, MessageModule, RippleModule, SocialLoginModule),
-       OrderService, CookieService,
+        importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, SharedModule, ButtonModule, ImageModule, ChipsModule, DropdownModule, FormsModule, CheckboxModule, MessageModule, RippleModule, SocialLoginModule),
+        PrimeNGConfig, OrderService, CookieService,
         { provide: HTTP_INTERCEPTORS, useClass: HttpIntercepterBaseAuthService, multi: true },
         {
             provide: 'SocialAuthServiceConfig',
