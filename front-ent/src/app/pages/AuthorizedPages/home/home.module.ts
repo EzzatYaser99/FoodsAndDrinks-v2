6cc30ef631872bbac71176997c3d1b7c64786d69
Ezzat Yaser <eyasser@ntgclarity.com>
2025-09-27 03:34:29 +0300

    Revert "fix some problems because of updated version"
    This reverts commit b5173b88

--- front-ent/src/app/pages/AuthorizedPages/home/home.module.ts
+++ front-ent/src/app/pages/AuthorizedPages/home/home.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 // import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home.component';
 // import {CarouselModule} from "primeng/carousel";
 // import {ButtonModule} from "primeng/button";
 // import {ToastModule} from "primeng/toast";
 import {FormsModule} from "@angular/forms";
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {CategoryService} from "../../../shared/services/category.service";
 import {CarouselModule} from "primeng/carousel";
 import {ButtonModule} from "primeng/button";
@@ -14,9 +16,9 @@ import {MainRoutingModule} from "../../main/main-routing.module";
 
 
 @NgModule({ declarations: [
-
+        HomeComponent
     ],
-    bootstrap: [], imports: [
+    bootstrap: [HomeComponent], imports: [CommonModule,
         HomeRoutingModule,
         CarouselModule,
         ButtonModule,
@@ -25,6 +27,6 @@ import {MainRoutingModule} from "../../main/main-routing.module";
         ToastModule,
         FormsModule,
         CarouselModule,
-        MainRoutingModule], providers: [CategoryService] })
+        MainRoutingModule], providers: [CategoryService, provideHttpClient(withInterceptorsFromDi())] })
 export class HomeModule {
 }
